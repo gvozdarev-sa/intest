0425b3043fc275dd7d5fd8f0d324ce59e2928923
gvozdarev-sa <gvozdarev.sa@gmail.com>
2013-10-05 18:25:23 +0400

    Added stuff for self testing

---
+++ stuff/user1/simple/main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+
+int main( int argc, char* argv[ ])
+{
+    if ( argc > 2)
+    {
+        int i;
+        for( i = 1; i < argc; i++)
+        {
+            printf( "%s ", argv[ i]);
+        }
+    }
+    else
+    {
+        return 1;
+    }
+    return 0;
+}
